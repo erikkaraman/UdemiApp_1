dec7294956ba8c32fedb636c5701c03d0cccd2d7
Erik <erikkaraman7@gmail.com>
2023-04-23 17:54:40 +0300

    new cycle

--- js/script.js
+++ js/script.js
@@ -8,12 +8,35 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('один з останніх переглянутих фільмів?', ''),
-      b = prompt('яку оцінку даси йому?', ''),
-      c = prompt('останній переглянутий фільм', ''),
-      d = prompt('один з останніх переглянутих фільмів?', '');
+// const a = prompt('один з останніх переглянутих фільмів?', ''),
+//       b = prompt('яку оцінку даси йому?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один з останніх переглянутих фільмів?', '');
+    const b = prompt('яку оцінку даси йому?', '');
+
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто замало фільмів');
+    stop;
+} else if (personalMovieDB.count < 30) {
+    console.log('Ви класний глядач');
+    stop;
+} else if (personalMovieDB.count > 30) {
+    console.log('Ви справжній кіноман');
+    stop;
+} else {
+    console.log('Error');
+};
 
 console.log(personalMovieDB);
